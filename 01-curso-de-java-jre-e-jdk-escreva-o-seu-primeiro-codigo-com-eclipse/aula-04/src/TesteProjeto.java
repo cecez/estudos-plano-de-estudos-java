2e44c987c752fb719e955d7bbf3ece506110bd46
Cezar Rosa <cecez86@gmail.com>
2021-10-26 11:07:34 -0300

    cadastro e listagem de tarefa

--- 01-curso-de-java-jre-e-jdk-escreva-o-seu-primeiro-codigo-com-eclipse/aula-04/src/TesteProjeto.java
+++ 01-curso-de-java-jre-e-jdk-escreva-o-seu-primeiro-codigo-com-eclipse/aula-04/src/TesteProjeto.java
@@ -1,11 +1,15 @@
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class TesteProjeto {
 
     public static final String OPCAO_SAIR = "1";
+    public static final String OPCAO_CADASTRAR = "2";
+    public static final String OPCAO_LISTAR = "5";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in); //System.in is a standard input stream
         System.out.print("Digite seu nome: ");
@@ -13,20 +17,38 @@ public class TesteProjeto {
         System.out.println("Olá " + str);
 
         System.out.println("Bem-vindo ao sisteminha para cadastro de tarefas (SPCT)");
-        System.out.println("Opções disponíveis: (1) Sair do sistema (2) Cadastrar tarefa (3) Concluir tarefa (4) Excluir tarefa (5) Listar tarefas");
 
         String opcao;
+        ArrayList<String> tarefas = new ArrayList<String>();
         do {
+            System.out.println("***********************************");
+            System.out.println("Opções disponíveis: (1) Sair do sistema (2) Cadastrar tarefa (3) Concluir tarefa (4) Excluir tarefa (5) Listar tarefas");
+            System.out.println("***********************************");
             System.out.print("Digite uma opção: ");
             opcao = sc.nextLine();
 
             switch (opcao) {
-                case OPCAO_SAIR:
-                    System.out.println("Você escolheu sair, adeus.");
-                    break;
-                default:
-                    System.out.println("Opção não existente. Tente novamente.");
+                case OPCAO_SAIR -> System.out.println("Você escolheu sair, adeus.");
+                case OPCAO_CADASTRAR -> {
+                    System.out.print("Digite o nome da tarefa: ");
+                    String nomeTarefa = sc.nextLine();
+                    tarefas.add(nomeTarefa);
+                }
+                case OPCAO_LISTAR -> {
+                    System.out.println("Listando tarefas:");
+                    if (tarefas.size() == 0) {
+                        System.out.println("Sem tarefas cadastradas");
+                        break;
+                    }
+                    for (int i = 0; i < tarefas.size(); i++) {
+                        System.out.println("[ ] " + i + ". " + tarefas.get(i));
+                    }
+                }
+                default -> System.out.println("Opção não existente. Tente novamente.");
             }
+
+            // TODO limpar tela
+
 
         } while (!Objects.equals(opcao, OPCAO_SAIR));
 
