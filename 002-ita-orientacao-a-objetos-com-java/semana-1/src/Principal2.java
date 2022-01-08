8d66b24353ffc67a1e2c862881557244de72b8db
Cezar Rosa <cecez86@gmail.com>
2022-01-08 10:23:15 -0300

    feat: imprimindo IMC dos pacientes

--- 002-ita-orientacao-a-objetos-com-java/semana-1/src/Principal2.java
+++ 002-ita-orientacao-a-objetos-com-java/semana-1/src/Principal2.java
@@ -3,11 +3,13 @@ public class Principal2 {
     public static void main(String[] args) {
         Paciente joaoPedro = new Paciente(18, 1.20);
         Paciente cezar = new Paciente(69, 1.74);
-        Paciente anaPaula = new Paciente(72, 1.70);
+        Paciente anaPaula = new Paciente(78, 1.70);
 
-        System.out.println("Diagnóstico do paciente João Pedro: " + joaoPedro.diagnostico());
-        System.out.println("Diagnóstico do paciente Cezar: " + cezar.diagnostico());
-        System.out.println("Diagnóstico da paciente Ana Paula: " + anaPaula.diagnostico());
+        System.out.println("Paciente João Pedro\n IMC: " + joaoPedro.calcularIMC() + "\n Diagnóstico: " + joaoPedro.diagnostico());
+        System.out.println("\n");
+        System.out.println("Paciente Cezar\n IMC: " + cezar.calcularIMC() + "\n Diagnóstico: " + cezar.diagnostico());
+        System.out.println("\n");
+        System.out.println("Paciente Ana Paula\n IMC: " + anaPaula.calcularIMC() + "\n Diagnóstico: " + anaPaula.diagnostico());
     }
 
 }
