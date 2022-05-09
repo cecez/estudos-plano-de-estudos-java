44ebbf0fd923df932298a829648fdc2cc3d25d5c
Cezar Castro Rosa <cecez86@gmail.com>
2022-05-09 14:45:13 -0300

    feat: deposit and withdraw methods and tests

--- 004-java-oo-introducao-a-orientacao-a-objetos/aula2/src/BankAccount.java
+++ 004-java-oo-introducao-a-orientacao-a-objetos/aula2/src/BankAccount.java
@@ -1,8 +1,21 @@
 public class BankAccount {
-    double balance;
+    double balance = 0;
     int agency;
     int number;
     String holder;
-    boolean active;
+    boolean active = true;
     char code;
+
+    public void deposit(double value) {
+        this.balance += value;
+    }
+
+    public boolean withdraw(double value) {
+        if (value > this.balance) {
+            return false;
+        }
+
+        this.balance -= value;
+        return true;
+    }
 }
--- 004-java-oo-introducao-a-orientacao-a-objetos/aula2/tests/BankAccountTest.java
+++ 004-java-oo-introducao-a-orientacao-a-objetos/aula2/tests/BankAccountTest.java
@@ -1,14 +1,56 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class BankAccountTest {
 
-    @Test
-    void test() {
-        BankAccount bankAccount = new BankAccount();
+    BankAccount bankAccount;
 
-        System.out.println(bankAccount.active);
+    @BeforeEach
+    void setUp() {
+        this.bankAccount = new BankAccount();
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.bankAccount = null;
+    }
+
+    @Test
+    void it_has_an_active_account() {
+        assertTrue(this.bankAccount.active);
+    }
+
+    @Test
+    void it_can_deposit_values() {
+        this.bankAccount.deposit(200);
+        this.bankAccount.deposit(200);
+
+        assertEquals(400, this.bankAccount.balance);
+    }
+
+    @Test
+    void it_can_withdraw_values() {
+        this.bankAccount.deposit(100);
+
+        assertTrue(this.bankAccount.withdraw(50));
+        assertEquals(50, this.bankAccount.balance);
+    }
+
+    @Test
+    void it_can_withdraw_an_zero_an_account() {
+        this.bankAccount.deposit(100);
+
+        assertTrue(this.bankAccount.withdraw(100));
+        assertEquals(0, this.bankAccount.balance);
+    }
+
+    @Test
+    void it_cannot_withdraw_values() {
+        assertFalse(this.bankAccount.withdraw(100));
+        assertEquals(0, this.bankAccount.balance);
     }
 
 }
