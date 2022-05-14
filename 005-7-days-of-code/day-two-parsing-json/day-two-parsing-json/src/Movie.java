d7411bcc15881ffdcf51deab849c82eff68c423c
Cezar Rosa <cecez86@gmail.com>
2022-05-14 20:29:07 -0300

    feat: parsing json data using Java String methods

---
+++ 005-7-days-of-code/day-two-parsing-json/day-two-parsing-json/src/Movie.java
@@ -0,0 +1,20 @@
+public class Movie {
+    private String title;
+    private String urlImage;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+}
