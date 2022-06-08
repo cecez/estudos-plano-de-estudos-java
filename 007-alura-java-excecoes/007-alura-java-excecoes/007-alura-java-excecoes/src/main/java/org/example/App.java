57b540bc46665cce8703a9250609d09dab9bf424
Cezar Castro Rosa <cecez86@gmail.com>
2022-06-08 14:36:14 -0300

    docs: simple code to use debugger

---
+++ 007-alura-java-excecoes/007-alura-java-excecoes/007-alura-java-excecoes/src/main/java/org/example/App.java
@@ -0,0 +1,29 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        method1();
+        System.out.println("Good bye");
+    }
+
+    private static void method1() {
+        System.out.println("Init method 1");
+        method2();
+        System.out.println("end method 1");
+    }
+
+    private static void method2() {
+        System.out.println("init method 2");
+        for (int i=1; i<=5; i++) {
+            System.out.println(i);
+        }
+        System.out.println("end method 2");
+    }
+}
